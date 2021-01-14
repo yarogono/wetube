a7c5c4d4c0f14dbc2006a64999e83619a617751a
Coker <naocya@naver.com>
2021-01-14 18:36:14 +0900

    #6.5

--- db.js
+++ db.js
@@ -5,6 +5,8 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
--- middlewares.js
+++ middlewares.js
@@ -4,13 +4,26 @@ import routes from "./routes";
 export const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localMiddleware = (req, res, next) => {
-  res.locals.siteName = "WeTube";
+  res.locals.siteName = "DogTube";
   res.locals.routes = routes;
-  res.locals.user = {
-    isAuthenticated: true,
-    id: 1,
-  };
+  res.locals.user = req.user || null;
   next();
+};
+
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
