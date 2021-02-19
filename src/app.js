0719379dfdacab1641f13ec7ec0275b743b876d6
yarogono <70641418+yarogono@users.noreply.github.com>
2021-02-19 20:33:10 +0900

    app.js

--- src/app.js
+++ src/app.js
@@ -28,8 +28,7 @@ app.use(
 );
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-// app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static(path.join(__dirname, "/static")));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
