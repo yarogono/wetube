1ccffe0526aed0742116e724aa86735b5f27faa3
yarogono <70641418+yarogono@users.noreply.github.com>
2021-02-21 10:18:00 +0900

    app.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -7,7 +7,6 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  devtool: false,
   mode: MODE ? "development" : "production",
   entry: ["@babel/polyfill", ENTRY_FILE],
   module: {
