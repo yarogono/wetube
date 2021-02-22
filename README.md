04a4cb27d43467e12dc9130e584b31c037de4068
yarogono <70641418+yarogono@users.noreply.github.com>
2021-02-23 00:31:25 +0900

    Done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wetube
 
 Cloning Youtube with Vanilla and NodeJS
+
+https://fathomless-reef-72658.herokuapp.com/
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
 import path from "path";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import { localMiddleware } from "./middlewares";
 import routes from "./routes";
@@ -41,6 +42,8 @@ app.use(
     store: new CokieStore({ mongooseConnection: mongoose.connection }),
   })
 );
+app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
